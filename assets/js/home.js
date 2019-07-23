f4e8af31858be320099a9702c3a1bdb3c5c78c73
José Tobias <jose.tobias@outlook.com>
2019-07-23 19:01:44 -0300

    Fixing mobile characteristics for navigation

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,32 +1,37 @@
-function hider(e, val) {
-    if (val == true) {
-        e.css("visibility", "visible");
-        e.fadeIn();
-    } else {
-        e.fadeOut();
-    }
+/* helper functions */
+function hider(e) {
+    e.fadeOut();
 }
 
-var point = $("#nav-full");
-var rollHead = $("#on-roll-head");
-var rollFoot = $("#on-roll-foot");
-var hh = $(window).scrollTop();
-
-if (hh > point.height()) {
-    hider(rollHead, true);
-    hider(rollFoot, true);
-} else {
-    hider(rollHead, false);
-    hider(rollFoot, false);
+function shower(e) {
+    e.css("visibility", "visible");
+    e.fadeIn();
 }
+/* */
 
+// scroll height marker
+var height = $(window).scrollTop();
+
+// entities to be managed
+const header = $("#nav-full");
+const rollHead = $("#on-roll-head");
+const rollFoot = $("#on-roll-foot");
+const triggerPoint = header.height() - rollHead.height();
+const entities = [rollHead, rollFoot];
+
+// if the page is reload in the middle
+(height > triggerPoint) ? entities.forEach(shower) : entities.forEach(hider);
+
+// scroll control
 $(window).scroll(function () {
-    var hh = $(window).scrollTop();
-    if (hh > point.height()) {
-        hider(rollHead, true);
-        hider(rollFoot, true);
-    } else {
-        hider(rollHead, false);
-        hider(rollFoot, false);
-    }
+    var height = $(window).scrollTop();
+    (height > triggerPoint) ? entities.forEach(shower) : entities.forEach(hider);
+});
+
+// scrollback on posts size
+var postSize = $("#achievments-list li").first().height();
+$("#on-roll-head a.nav-link").on("click", function (e) {
+    e.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -postSize);
 });
