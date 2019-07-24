413bbfc3401cb21597382f42a09c13da73537f4a
José Tobias <jose.tobias@outlook.com>
2019-07-23 22:32:15 -0300

    Changed items styling and JQuery rules, also fixed a bug with viewheight for the scrollspy class

--- assets/js/home.js
+++ assets/js/home.js
@@ -16,26 +16,40 @@ var height = $(window).scrollTop();
 const header = $("#nav-full");
 const rollHead = $("#on-roll-head");
 const rollFoot = $("#on-roll-foot");
-const triggerPoint = header.height() - (rollHead.height() + 1);
+const triggerPoint = header.height() - (rollHead.height());
 const entities = [rollHead, rollFoot];
 
 // if the page is reload in the middle
-(height >= triggerPoint) ? entities.forEach(shower) : entities.forEach(hider);
+(height > triggerPoint) ? entities.forEach(shower) : entities.forEach(hider);
 
 // scroll control
 $(window).scroll(function () {
     var height = $(window).scrollTop();
-    (height >= triggerPoint) ? entities.forEach(shower) : entities.forEach(hider);
+    (height > triggerPoint) ? entities.forEach(shower) : entities.forEach(hider);
 });
 
 // scrollback on posts size
-var scrollBack = rollHead.height();
-$("#on-roll-head a.nav-link").on("click", function (e) {
+const scrollBackHead = rollHead.height();
+const scrollBackItem = $("#achievments-list li.list-group-item").first().height();
+
+const onRollFirst = $("#on-roll-head a.nav-link").first();
+const onRollRest = $("#on-roll-head a.nav-link").slice(1, $("#on-roll-head li.nav-item").length);
+
+
+onRollFirst.on("click", function (e) {
     e.preventDefault();
     $($(this).attr('href'))[0].scrollIntoView();
-    scrollBy(0, -scrollBack);
+    scrollBy(0, -scrollBackHead);
 });
 
+onRollRest.on("click", function (e) {
+    console.log(scrollBackItem);
+    e.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -scrollBackItem);
+});
+
+
 // scrollspy activation offset
-const offSet = rollHead.height();
+const offSet = $(window).height() * 0.2;
 $('body').scrollspy({ target: '#spied-body', offset: offSet });
